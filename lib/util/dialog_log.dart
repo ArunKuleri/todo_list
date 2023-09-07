68d74b6ec33f78d0856b4e7c340bb03156de0ec8
Arun <arunk.aru1@gmail.comgit config --global user.name Arungit commit -m demogit config --global user.email arunk.aru1@gmail.com>
2023-09-07 14:54:34 +0530

    made changes in to-do app added validator

--- lib/util/dialog_log.dart
+++ lib/util/dialog_log.dart
@@ -8,39 +8,61 @@ class DialogBox extends StatelessWidget {
   VoidCallback onSave;
   VoidCallback onCancel;
   DialogBox({
-    super.key,
+    Key? key,
     required this.controller,
     required this.onSave,
     required this.onCancel,
-  });
+  }) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-        backgroundColor: Colors.blueAccent[300],
-        content: SizedBox(
-          height: 120,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Add a new Task",
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: AlertDialog(
+          backgroundColor: Colors.blueAccent[300],
+          content: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  MyButton(text: 'save', onPressed: onSave),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  MyButton(text: 'cancel', onPressed: onCancel),
+                  TextFormField(
+                      controller: controller,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Add a new Task",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'please enter some text';
+                        }
+                        return null;
+                      }),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      MyButton(
+                          text: 'save',
+                          onPressed: () {
+                            if (_formKey.currentState?.validate() ?? false) {
+                              onSave();
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text("pleease enter a text")));
+                            }
+                          }),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      MyButton(text: 'cancel', onPressed: onCancel),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        ));
+              ),
+            ),
+          )),
+    );
   }
 }
